47229ee98abc37889e8ea4b73f5d92c8ce10dcff
Nurbek <nurbekturanov0@gmail.com>
2023-11-20 19:07:41 +0500

    snake-game-better

--- snake-game/main.py
+++ snake-game/main.py
@@ -34,15 +34,15 @@ while game_is_on:
         scoreboard.increase_score()
 
     # Detect collisions with wall.
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        scoreboard.reset()
+        snake.reset()
 
     # Detect collisions with tail.
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 
 screen.exitonclick()
